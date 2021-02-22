f1943dfa6904cb631efddc6cb4d3d69788f61eb0
NashidForbes <NashidForbes@biohumans.com>
2021-02-22 13:14:10 -0500

    Using a junit4 test and using Mockito for services and repository.

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() throws Exception {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long id = Long.valueOf(41);
+        category.setId(id);
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    public void getVersion() {
+    }
+
+    @Test
+    public void getDepartmentName() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
