8e626455e1abdfcc2664170859aba0fca4ba9117
NashidForbes <NashidForbes@biohumans.com>
2021-02-22 14:56:05 -0500

    Using a junit4 test and using Mockito for services and repository. Adding IndexController test.  Removed unnecessary parameters in index controller.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -15,16 +15,9 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final RecipeService recipeService;
 
-    public IndexController(
-            CategoryRepository categoryRepository,
-            UnitOfMeasureRepository unitOfMeasureRepository,
-            RecipeService recipeService) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
@@ -32,16 +25,7 @@ public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
         log.debug("Request sent for index page");
-/*        Optional<Category> categoryOptional = categoryRepository.findByDepartmentName(
-                "American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional =
-                unitOfMeasureRepository.findByUnits("Teaspoon");*/
-
         model.addAttribute("recipes", recipeService.getRecipes());
-
-/*        System.out.println("Cat id is: " + categoryOptional.get().getId());
-        System.out.println("Uom id is " + unitOfMeasureOptional.get().getId());*/
-
         return "index";
     }
 }
