bff0235001d19177d57d7f5448b7f575e2aaddf2
NashidForbes <NashidForbes@biohumans.com>
2021-04-25 00:24:38 -0400

    Added new configuration bean to add a router function to return JSON response for recipeService for getRecipes().

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -1,22 +1,27 @@
 package guru.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
+// a global handler for bind exceptions
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
     // need to declare @ResponseStatus as MVC conventional exception handling takes
     // precedence
-/*    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-        log.error("Number format exception");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model){
+        log.error("Handling Binding Exception");
         log.error("exception: " + exception.getMessage());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
 
-        return modelAndView;
-    }*/
+        model.addAttribute("exception", exception);
+
+        return "404error";
+    }
 }
